5abc39790d256c258276d6a0bf2e04c9624fb75f
Daniel Coorey <daniel.coorey@gmail.com>
2014-12-10 15:08:57 +1000

    Initial commit

---
+++ ShoppingTest/Checkout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingTest
+{
+    class Checkout
+    {
+        private Dictionary<char, int> prices;
+
+        public Checkout(Dictionary<char, int> prices)
+        {
+            this.prices = prices;
+        }
+        internal void Scan(char itemCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Total { get; set; }
+    }
+}
---
+++ ShoppingTest/CheckoutTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ShoppingTest
+{
+    [TestClass]
+    public class CheckoutTest
+    {
+        [TestMethod]
+        public void TotalShouldBeSumOfScannedItemPrices()
+        {
+            var prices = new Dictionary<char, int>();
+            prices.Add('A', 50);
+            prices.Add('B', 45);
+
+            //var specials = new List<Tuple<char,int, int>>();
+            //specials.Add(new Tuple<char, int, int>('A', 3, 130));
+            //specials.Add(new Tuple<char, int, int>('A', 4, 160));
+            //specials.Add(new Tuple<char, int, int>('B', 2, 80));
+
+            Checkout checkout = new Checkout(prices);//, specials);
+            checkout.Scan('A');
+            checkout.Scan('B');
+            Assert.AreEqual(95, checkout.Total);
+        }
+    }
+}
